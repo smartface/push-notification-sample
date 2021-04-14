b8c77d9ac03e38cfa165cce34ee6ce7149153aa2
furkan.arabaci <furkan.arabaci@smartface.io>
2021-04-14 09:46:29 +0000

    initial Smartface commit

---
+++ scripts/routes/index.ts
@@ -0,0 +1,35 @@
+import buildExtender from "sf-extension-utils/lib/router/buildExtender";
+import {
+    NativeRouter as Router,
+    NativeStackRouter as StackRouter,
+    Route
+} from "@smartface/router";
+import "sf-extension-utils/lib/router/goBack"; // Implements onBackButtonPressed
+
+const router = Router.of({
+    path: "/",
+    isRoot: true,
+    routes: [
+        StackRouter.of({
+            path: "/pages",
+            routes: [
+                Route.of({
+                    path: "/pages/page1",
+                    build: buildExtender({ 
+                        getPageClass: () => require("pages/page1").default, 
+                        headerBarStyle: { visible: true } 
+                    })
+                }),
+                Route.of({
+                    path: "/pages/page2",
+                    build: buildExtender({ 
+                        getPageClass: () => require("pages/page2").default, 
+                        headerBarStyle: { visible: true } 
+                    })
+                }),
+            ]
+        })
+    ]
+});
+
+export default router;
