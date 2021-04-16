b968c0d16db8da6a89f2ea3eaefdb6c2002b9cbe
furkan.arabaci <furkan.arabaci@smartface.io>
2021-04-16 07:41:47 +0000

    delete default pages

--- scripts/pages/page1.ts
+++ scripts/pages/page1.ts
@@ -5,38 +5,18 @@ import System from "sf-core/device/system";
 
 export default class Page1 extends Page1Design {
     router: any;
-	constructor () {
+    constructor() {
         super();
-		// Overrides super.onShow method
         this.onShow = onShow.bind(this, this.onShow.bind(this));
-		// Overrides super.onLoad method
-		this.onLoad = onLoad.bind(this, this.onLoad.bind(this));
-        this.btnNext.onPress = () => {
-            this.router.push("/pages/page2", { message: "Hello World!" });
-        }
+        this.onLoad = onLoad.bind(this, this.onLoad.bind(this));
     }
 }
 
-/**
- * @event onShow
- * This event is called when a page appears on the screen (everytime).
- */
 function onShow(superOnShow: () => void) {
-  superOnShow();
-  this.headerBar.titleLayout.applyLayout();
+    superOnShow();
 }
 
-/**
- * @event onLoad
- * This event is called once when page is created.
- */
+
 function onLoad(superOnLoad: () => void) {
     superOnLoad();
-    console.info('Onload page1');
-    this.headerBar.leftItemEnabled = false;
-    this.headerBar.titleLayout = new PageTitleLayout();
-    componentContextPatch(this.headerBar.titleLayout, "titleLayout");
-    if (System.OS === "Android") {
-        this.headerBar.title = "";
-    }
 }
