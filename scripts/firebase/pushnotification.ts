17e1c12d0d104c7ab96100e747b7754e5f173fe2
furkan.arabaci <furkan.arabaci@smartface.io>
2021-05-07 07:09:41 +0000

    add visuals

--- scripts/firebase/pushnotification.ts
+++ scripts/firebase/pushnotification.ts
@@ -2,27 +2,39 @@ import Application from "sf-core/application";
 import Firebase from 'sf-plugin-firebase';
 import Notifications from 'sf-core/global/notifications';
 
+let firebaseToken = '';
+let fcmToken = '';
+
+export function getFirebaseToken(): string {
+	return firebaseToken;
+}
+
+export function getFcmToken(): string {
+	return fcmToken;
+}
+
 Notifications.onNotificationReceive = (e) => {
-    const alertMessage = {
-        'onNotificationReceive': e
-    }
-    alert(JSON.stringify(e, null, 4));
-    return [Notifications.iOS.NotificationPresentationOptions.SOUND, Notifications.iOS.NotificationPresentationOptions.ALERT]; // or []
+	const alertMessage = {
+		'onNotificationReceive': e
+	}
+	alert(JSON.stringify(e, null, 4));
+	return [Notifications.iOS.NotificationPresentationOptions.SOUND, Notifications.iOS.NotificationPresentationOptions.ALERT]; // or []
 };
 
 Firebase.messaging.subscribeToTopic("all"); //this triggers register for notifications
 
 Notifications.registerForPushNotifications(
-    //@ts-ignore
-    (e: { token: string }): void => {
-        //@ts-ignore
-        Firebase.messaging.getToken((e) => {
-            alert("Firebase Token : " + JSON.stringify(e, null, '\t'));
-        })
-        //@ts-ignore
-        alert("FCM Token : " + JSON.stringify(Firebase.messaging.ios.native.FCMToken(), null, '\t'))
-        alert("Successfully registered. The token is: " + e.token);
-    },
-    (): void => {
-        alert("Register failed.");
-    });
+	//@ts-ignore
+	(e: { token: string }): void => {
+		//@ts-ignore
+		Firebase.messaging.getToken((e) => {
+			alert("FCM Token : " + JSON.stringify(e, null, '\t'));
+			fcmToken = e;
+		})
+		//@ts-ignore
+		alert("Successfully registered. The token is: " + e.token);
+		firebaseToken = e.token;
+	},
+	(): void => {
+		alert("Register failed.");
+	});
--- scripts/pages/page1.ts
+++ scripts/pages/page1.ts
@@ -1,19 +1,28 @@
 import Page1Design from 'generated/pages/page1';
 
+import * as PushNotification from 'firebase/pushnotification';
+
 export default class Page1 extends Page1Design {
-    router: any;
-    constructor() {
-        super();
-        this.onShow = onShow.bind(this, this.onShow.bind(this));
-        this.onLoad = onLoad.bind(this, this.onLoad.bind(this));
-    }
+	router: any;
+	constructor() {
+		super();
+		this.onShow = onShow.bind(this, this.onShow.bind(this));
+		this.onLoad = onLoad.bind(this, this.onLoad.bind(this));
+		this.btnGetTokens.onPress = () => this.initLabels();
+	}
+
+	initLabels() {
+		this.lblFcmToken.text = `Fcm Token : ${PushNotification.getFcmToken()}`;
+		this.lblFirebaseToken.text = `Firebase Token : ${PushNotification.getFirebaseToken()}`;
+	}
 }
 
 function onShow(superOnShow: () => void) {
-    superOnShow();
+	superOnShow();
+
 }
 
 
 function onLoad(superOnLoad: () => void) {
-    superOnLoad();
+	superOnLoad();
 }
