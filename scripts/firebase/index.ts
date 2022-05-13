5a54d03265745292db7cf8602c84e33f091ee7da
alimoncul <alimoncul@gmail.com>
2022-05-13 09:22:43 +0300

    add try catch

--- scripts/firebase/index.ts
+++ scripts/firebase/index.ts
@@ -2,47 +2,28 @@ import System from "@smartface/native/device/system";
 import File from "@smartface/native/io/file";
 import Firebase, { Crashlytics } from "@smartface/plugin-firebase";
 import Path from "@smartface/native/io/path";
-import Invocation from "@smartface/native/util/iOS/invocation.js";
 
-const config = System.OS === System.OSType.IOS && {
-  iosFile: new File({
-    path: `${Path.AssetsUriScheme}GoogleService-Info.plist`,
-  }),
-};
+try {
+  const config = System.OS === System.OSType.IOS && {
+    iosFile: new File({
+      path: `${Path.AssetsUriScheme}GoogleService-Info.plist`,
+    }),
+  };
 
-/**
- * WORKAROUND: SUPDEV-2372
- * Removes clipboard entry on first app open to prevent alert showing up
- */
-
-if (System.OS === "iOS") {
-  const arg1 = new Invocation.Argument({
-    type: "NSString",
-    value: "",
-  });
-
-  const arg2 = new Invocation.Argument({
-    type: "NSString",
-    value: "com.apple.UIKit.pboard.general",
-  });
-  Invocation.invokeInstanceMethod(
-    global.__SF_UIPasteboard.generalPasteboard(),
-    "setValue:forPasteboardType:",
-    [arg1, arg2]
-  );
-}
-
-if (Firebase.apps().length === 0) {
-  const firebaseApp = Firebase.initializeApp(config);
-  const auth = firebaseApp.auth();
-  auth?.createUserWithEmailAndPassword(
-    "test@testmail.com",
-    "testPass123!.",
-    () => {}
-  );
-  const user = auth?.getCurrentUser();
-  console.log("User's Email", user?.getEmail());
-  Crashlytics.ios.with([new Crashlytics()]);
+  if (Firebase.apps().length === 0) {
+    const firebaseApp = Firebase.initializeApp(config);
+    const auth = firebaseApp.auth();
+    auth?.createUserWithEmailAndPassword(
+      "test@testmail.com",
+      "testPass123!.",
+      () => {}
+    );
+    const user = auth?.getCurrentUser();
+    console.log("User's Email", user?.getEmail());
+    Crashlytics.ios.with([new Crashlytics()]);
+  }
+} catch (error) {
+  alert(error.message, { stack: error.stack });
 }
 
 import "./pushnotification";
